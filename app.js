262bac2a9d2230391b7bff48eeb8198e40c75ffe
Massimo <massimo.rosas279@gmail.com>
2020-10-22 12:35:55 +0200

    Created queries folder + errorhandler

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const path = require('path');
 const index = require('./routes');
 require('./database');
+const errorHandler = require('errorhandler');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -16,5 +17,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(index);
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(errorHandler());
+} else {
+  app.use((err, req, res, next) => {
+    const code = err.code || 500;
+    res.status(code).json({
+      code: code,
+      message: code === 500 ? null : err.message
+    });
+  })
+}
 
 app.listen(port);
