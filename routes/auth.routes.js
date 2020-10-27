cf79bc2cbb2652ba9d427206f971431b0200419a
Massimo <massimo.rosas279@gmail.com>
2020-10-27 15:22:58 +0100

    Topbar and tweet refactoring

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,6 +3,6 @@ const { signinForm, signin, signout } = require('../controllers/auth.controller'
 
 router.get('/signin/form', signinForm);
 router.post('/signin', signin);
-router.delete('/signout', signout);
+router.get('/signout', signout);
 
 module.exports = router;
