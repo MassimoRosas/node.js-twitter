4e72a605729667dc6b3012dcf3f0145a6f222d57
Massimo <massimo.rosas279@gmail.com>
2020-10-27 14:45:14 +0100

    Added view and controller for login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,26 @@
-exports.singinForm = (req, res, next) => {
-  res.end();
+const passport = require("passport");
+
+exports.signinForm = (req, res, next) => {
+  res.render('auth/auth-form', { errors: null });
 };
 
-exports.singin = (req, res, next) => {
-  res.end();
+exports.signin = (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      next(err);
+    } else if (!user) {
+      res.render('auth/auth-form', { errors: [ info.message ]});
+    } else {
+      req.login(user, (err) => {
+        if (err) { next(err) } else {
+          res.redirect('/tweets');
+        }
+      })
+    }
+  })(req, res, next);
 };
 
-exports.singout = (req, res, next) => {
-  res.end();
+exports.signout = (req, res, next) => {
+  req.logout();
+  res.redirect('/auth/signin-form');
 };
