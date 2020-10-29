cdc8c58fc5bfbf2d3bb5b01457d99ac4d06381f3
Massimo <massimo.rosas279@gmail.com>
2020-10-29 15:31:39 +0100

    Update prod server

--- environment/production.js
+++ environment/production.js
@@ -1,5 +1,7 @@
 module.exports = {
   dbUrl: 'mongodb+srv://alex:aze@cluster0.lpnpa.gcp.mongodb.net/twitter?retryWrites=true&w=majority',
   cert: '/etc/letsencrypt/live/www.dyma-twitter-project.site/fullchain.pem',
-  key: '/etc/letsencrypt/live/www.dyma-twitter-project.site/privkey.pem'
+  key: '/etc/letsencrypt/live/www.dyma-twitter-project.site/privkey.pem',
+  portHttp: 80,
+  portHttps: 443
 }
