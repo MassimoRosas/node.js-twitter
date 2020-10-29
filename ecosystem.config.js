792d7b16d9d9a3aed6991d7b8eb0df46bacdcff3
Massimo <massimo.rosas279@gmail.com>
2020-10-29 18:33:34 +0100

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,14 +3,13 @@ module.exports = {
     name: 'twitter',
     instances: 'max',
     autorestart: true,
+    script: './bin/www',
+    watch: true,
     env: {
       NODE_ENV: 'development'
     },
     env_production: {
       NODE_ENV: 'production'
     }
-  }, {
-    script: './bin/www',
-    watch: true,
   }],
 };
