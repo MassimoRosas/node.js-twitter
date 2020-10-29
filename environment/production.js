bd396cf3d9c480b139c08367513dc03bd1b777ec
Massimo <massimo.rosas279@gmail.com>
2020-10-29 17:19:01 +0100

    update

--- environment/production.js
+++ environment/production.js
@@ -4,6 +4,6 @@ module.exports = {
   dbUrl: fs.readFileSync('mongodb+srv://alex:aze@cluster0.lpnpa.gcp.mongodb.net/twitter?retryWrites=true&w=majority'),
   cert: fs.readFileSync('/etc/letsencrypt/live/www.dyma-twitter-project.site/fullchain.pem'),
   key: fs.readFileSync('/etc/letsencrypt/live/www.dyma-twitter-project.site/privkey.pem'),
-  portHttp: 81,
+  portHttp: 80,
   portHttps: 443
 }
